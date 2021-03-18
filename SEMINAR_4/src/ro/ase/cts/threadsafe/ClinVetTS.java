b2645ed22b63fca283225a571e1875ec6b2a12cd
TGA-ASE <79546838+TGA-ASE@users.noreply.github.com>
2021-03-18 08:09:21 +0200

    Added THREADSAFE

--- SEMINAR_4/src/ro/ase/cts/threadsafe/ClinVetTS.java
+++ SEMINAR_4/src/ro/ase/cts/threadsafe/ClinVetTS.java
@@ -8,6 +8,65 @@ public class ClinVetTS {
 	private float buget;
 	
 	
+	private  static ClinVetTS instanta=null;
+
+
+	private ClinVetTS(String nume, String adresa, int nrDoctori, float buget) {
+		super();
+		this.nume = nume;
+		this.adresa = adresa;
+		this.nrDoctori = nrDoctori;
+		this.buget = buget;
+	}
+
+
+	public String getNume() {
+		return nume;
+	}
+
+
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+
+
+	public String getAdresa() {
+		return adresa;
+	}
+
+
+	public void setAdresa(String adresa) {
+		this.adresa = adresa;
+	}
+
+
+	public int getNrDoctori() {
+		return nrDoctori;
+	}
+
+
+	public void setNrDoctori(int nrDoctori) {
+		this.nrDoctori = nrDoctori;
+	}
+
+
+	public float getBuget() {
+		return buget;
+	}
+
+
+	public void setBuget(float buget) {
+		this.buget = buget;
+	}
+	
+	public static synchronized ClinVetTS getInstanta(String nume, String adresa, int nrDoctori, float buget)
+	{
+		if(instanta==null)
+			instanta=new ClinVetTS(nume,adresa,nrDoctori,buget);
+		return instanta;
+		
+	}
+	
 	
 
 }
