856c7bb32f8d9cfab17c91ac26fe423e8fa4b827
TGA-ASE <79546838+TGA-ASE@users.noreply.github.com>
2021-03-18 07:43:02 +0200

    Eager init

---
+++ SEMINAR_4/src/module-info.java
@@ -0,0 +1,2 @@
+module SEMINAR_4 {
+}
---
+++ SEMINAR_4/src/ro/ase/cts/ClinicaVeterinara.java
@@ -0,0 +1,39 @@
+package ro.ase.cts;
+
+public class ClinicaVeterinara {
+	private String nume;
+	private String adresa;
+	private int nrDoctori;
+	private float buget;
+	public String getNume() {
+		return nume;
+		
+		
+	}
+	private static final ClinicaVeterinara instanta = new ClinicaVeterinara();
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+	public String getAdresa() {
+		return adresa;
+	}
+	public void setAdresa(String adresa) {
+		this.adresa = adresa;
+	}
+	public int getNrDoctori() {
+		return nrDoctori;
+	}
+	public void setNrDoctori(int nrDoctori) {
+		this.nrDoctori = nrDoctori;
+	}
+	public float getBuget() {
+		return buget;
+	}
+	public void setBuget(float buget) {
+		this.buget = buget;
+	}
+	public static ClinicaVeterinara getInstanta(String nume ,String adresa, int nrDoctori, float capital) {
+		return instanta;
+	}
+	
+}
